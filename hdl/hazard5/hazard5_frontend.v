369375319a0bf6134f7d73c52a62e00c338b8b9c
Luke Wren <wren6991@gmail.com>
2018-11-04 00:21:52 +0000

    Frontend finally starting to look plausible. Verilator linting found a couple of actual bugs.
    Former-commit-id: 5e287a3b6b7cabc3a783b3416cf6117db1dca6d8
    Former-commit-id: cdc8e51bf7674a9fb412be35e0f277b1e9a75e59

--- hdl/hazard5/hazard5_frontend.v
+++ hdl/hazard5/hazard5_frontend.v
@@ -35,7 +35,7 @@ module hazard5_frontend #(
 	// This works OK because size is decoded from 2 LSBs of instruction, so cheap.
 	output reg  [31:0]       cir,
 	output reg  [1:0]        cir_vld, // number of valid halfwords in CIR
-	input wire  [1:0]        cir_req  // number of halfwords D will have room for
+	input wire  [1:0]        cir_use  // number of halfwords D intends to consume
 );
 
 
@@ -48,11 +48,11 @@ module hazard5_frontend #(
 //synthesis translate_off
 initial if (W_DATA != 32) begin $error("Frontend requires 32-bit databus"); end
 initial if ((1 << $clog2(FIFO_DEPTH)) != FIFO_DEPTH) begin $error("Frontend FIFO depth must be power of 2"); end
-initial if (!FIFO_DEPTH) begin $error("Frontend FIFO depth must be > 0"); end
+initial if (~|FIFO_DEPTH) begin $error("Frontend FIFO depth must be > 0"); end
 //synthesis translate_on
 
 localparam W_BUNDLE = W_DATA / 2;
-localparam W_FIFO_PTR = $clog2(FIFO_DEPTH + 1)
+localparam W_FIFO_PTR = $clog2(FIFO_DEPTH + 1);
 
 // ----------------------------------------------------------------------------
 // Fetch Queue (FIFO)
@@ -64,7 +64,7 @@ reg [W_FIFO_PTR-1:0] fifo_wptr;
 reg [W_FIFO_PTR-1:0] fifo_rptr;
 
 wire [W_FIFO_PTR-1:0] fifo_level = fifo_rptr - fifo_wptr;
-wire fifo_full = fifo_wptr ^ fifo_rptr == (1'b1 & {W_FIFO_PTR{1'b1}}) << (W_FIFO_PTR - 1);
+wire fifo_full = (fifo_wptr ^ fifo_rptr) == (1'b1 & {W_FIFO_PTR{1'b1}}) << (W_FIFO_PTR - 1);
 wire fifo_empty = fifo_wptr == fifo_rptr;
 wire fifo_almost_full = fifo_level == FIFO_DEPTH - 1;
 
@@ -82,7 +82,7 @@ always @ (posedge clk or negedge rst_n) begin
 		`ASSERT(!(fifo_push && fifo_full));
 		if (fifo_push) begin
 			fifo_wptr <= fifo_wptr + 1'b1;
-			fifo_mem[fifo_wptr] <= fifo_wdata;
+			fifo_mem[fifo_wptr & ~FIFO_DEPTH] <= fifo_wdata;
 		end
 		if (jump_target_vld) begin
 			fifo_rptr <= fifo_wptr;
@@ -92,7 +92,7 @@ always @ (posedge clk or negedge rst_n) begin
 	end
 end
 
-assign fifo_rdata = fifo_mem[fifo_rptr];
+assign fifo_rdata = fifo_mem[fifo_rptr & ~FIFO_DEPTH];
 
 // ----------------------------------------------------------------------------
 // Fetch logic
@@ -116,7 +116,7 @@ always @ (posedge clk or negedge rst_n) begin
 		if (jump_target_vld && jump_target_rdy) begin
 			// If the jump request goes straight to the bus, exclude from flush count
 			ctr_flush_pending <= pending_fetches_next - !mem_addr_hold;
-		end else if (ctr_flush_pending && mem_data_vld) begin
+		end else if (|ctr_flush_pending && mem_data_vld) begin
 			ctr_flush_pending <= ctr_flush_pending - 1'b1;
 		end
 	end
@@ -130,7 +130,7 @@ always @ (posedge clk or negedge rst_n) begin
 		fetch_addr <= {W_ADDR{1'b0}}; // TODO: reset vectoring
 	end else begin
 		if (jump_target_vld && jump_target_rdy) begin
-			fetch_addr <= {jump_target[W_ADDR-1:2], 2'b00} + (mem_addr_rdy ? 3'h4 : 3'h0);
+			fetch_addr <= {jump_target[W_ADDR-1:2] + mem_addr_rdy, 2'b00};
 		end else if (mem_addr_vld && mem_addr_rdy) begin
 			fetch_addr <= fetch_addr + 3'h4;
 		end
@@ -140,7 +140,7 @@ end
 // Using the non-registered version of pending_fetches would improve FIFO
 // utilisation, but create a combinatorial path from hready to address phase!
 wire fetch_stall = fifo_full
-	|| fifo_almost_full && pending_fetches
+	|| fifo_almost_full && |pending_fetches
 	|| pending_fetches > 2'h1;
 
 
@@ -160,7 +160,7 @@ always @ (posedge clk or negedge rst_n) begin
 		if (mem_addr_rdy) begin
 			unaligned_jump_aph <= 1'b0;
 		end
-		if (mem_data_vld && !ctr_flush_pending) begin
+		if (mem_data_vld && ~|ctr_flush_pending) begin
 			unaligned_jump_dph <= 1'b0;
 		end
 		if (unaligned_jump_now) begin
@@ -187,26 +187,8 @@ assign jump_target_rdy = !mem_addr_hold;
 
 
 // ----------------------------------------------------------------------------
-// Instruction assembly yard (IMUX)
+// Instruction assembly yard
 
-/*------------+---------------------------------+-----------------------------------+-----------------------------------+
-| buf  \  req |                0                |               1                   |             2                     |
-+-------------+---------------------------------+-----------------------------------+-----------------------------------+
-|      0      | Decode stalled, don't touch     | Shouldn't happen                  | Fill empty CIR                    |
-|             |                                 | (assert this)                     |                                   |
-+-------------+---------------------------------+-----------------------------------+-----------------------------------+
-|      1      | Decode stalled, don't touch     | Tried to decode half of 32b.      | Decoded 16b instruction.          |
-|             |                                 | Need topup.                       | Want full refill.                 |
-+-------------+---------------------------------+-----------------------------------+-----------------------------------+
-|      2      | Decode stalled, don't touch     | Decoded lower half as 16b.        | Full 32-bit CIR decoded.          |
-|             |                                 | Shift and topup.                  | Want full refill.                 |
-+-------------+---------------------------------+-----------------------------------+-----------------------------------+
-|      3      | Decode stalled, don't touch     | Decoded lower hw as 16b.          | Full 32-bit CIR decoded.          |
-|             |                                 | Shift and topup.                  | Shift in from hwbuf and backfill. |
-+-------------+---------------------------------+-----------------------------------+----------------------------------*/
-
-wire [W_DATA-1:0] fetch_data = fifo_empty ? mem_data : fifo_rdata;
-wire fetch_data_vld = !fifo_empty || (mem_data_vld && !ctr_flush_pending);
 
 // buf_level is the number of valid halfwords in {hwbuf, cir}.
 // cir_vld and hwbuf_vld are functions of this.
@@ -214,13 +196,31 @@ reg [1:0] buf_level;
 reg [W_BUNDLE-1:0] hwbuf;
 reg hwbuf_vld;
 
-wire [1:0] instr_consumption = cir_req <= cir_vld ? cir_req : cir_vld;
-wire buf_level_next =
-	jump_target_vld || ctr_flush_pending ? 2'h0 :
+// Shift any recycled instruction data down to backfill D's consumption
+// We don't care about anything which is invalid or will be overlaid with fresh data,
+// so choose these values in a way that minimises muxes
+wire [3*W_BUNDLE-1:0] instr_data_shifted =
+	cir_use[1] ? {hwbuf, cir[W_BUNDLE +: W_BUNDLE], hwbuf} :
+	cir_use[0] ? {hwbuf, hwbuf, cir[W_BUNDLE +: W_BUNDLE]} :
+	             {hwbuf, cir};
+
+wire [W_DATA-1:0] fetch_data = fifo_empty ? mem_data : fifo_rdata;
+wire fetch_data_vld = !fifo_empty || (mem_data_vld && ~|ctr_flush_pending);
+
+// Overlay fresh fetch data onto the shifted/recycled instruction data
+// Again, if something won't be looked at, generate cheapest possible garbage.
+// Don't care if fetch data is valid or not, as will just retry next cycle (as long as flags set correctly)
+wire [1:0] level_next_no_fetch = buf_level - cir_use;
+wire [3*W_BUNDLE-1:0] instr_data_plus_fetch =
+	unaligned_jump_dph     ? {instr_data_shifted[W_BUNDLE +: 2*W_BUNDLE], fetch_data[0 +:W_BUNDLE]} :
+	level_next_no_fetch[1] ? instr_data_shifted :
+	level_next_no_fetch[0] ? {fetch_data, instr_data_shifted[0 +: W_BUNDLE]} :
+	                         {instr_data_shifted[2*W_BUNDLE +: W_BUNDLE], fetch_data};
+
+wire [1:0] buf_level_next =
+	jump_target_vld || |ctr_flush_pending ? 2'h0 :
 	fetch_data_vld && unaligned_jump_dph ? 2'h1 :
-	buf_level + {fetch_data_vld, 1'b0} - instr_consumption;
-
-
+	buf_level + {fetch_data_vld, 1'b0} - cir_use;
 
 always @ (posedge clk or negedge rst_n) begin
 	if (!rst_n) begin
@@ -231,103 +231,15 @@ always @ (posedge clk or negedge rst_n) begin
 		hwbuf <= {W_BUNDLE{1'b0}};
 	end else begin
 		`ASSERT(cir_vld <= 2);
-		`ASSERT(cir_req <= 2);
+		`ASSERT(cir_use <= 2);
+		`ASSERT(cir_use <= cir_vld);
 		`ASSERT(!(cir_vld == 0 && cir_req == 1));
 		// Update CIR flags
 		buf_level <= buf_level_next;
 		cir_vld <= buf_level_next & ~(buf_level_next >> 1'b1);
 		hwbuf_vld <= &buf_level_next;
 		// Update CIR contents
-		if (cir_req) begin
-			// LSBs
-			casez ({unaligned_jump_dph, cir_req, buf_level})
-				5'b1_??_?? : cir[0 +: W_BUNDLE] <= fetch_data[W_BUNDLE +: W_BUNDLE];
-				5'b0_?1_1? : cir[0 +: W_BUNDLE] <= cir[W_BUNDLE +: W_BUNDLE];
-                5'b0_1?_11 : cir[0 +: W_BUNDLE] <= hwbuf;
-                5'b0_1?_?? : cir[0 +: W_BUNDLE] <= fetch_data[0 +: W_BUNDLE];
-				default: begin end
-			endcase
-			// MSBs
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// CIR LSBs have 4 sources, on cycles where they change:
-// - CIR MSBs (instr was 16 bit)
-// - Halfword buffer (instr was 32 bit and hwb valid)
-// - Fetch LSBs (instr was 32 bit and hwb nonvalid)
-// - Fetch MSBs (unaligned jump)
-// CIR MSBs have 3:
-// - Halfword buffer (instr was 16-bit and hwb valid)
-// - Fetch LSBs (16-bit instr and hwb nonvalid, 32-bit and hwb valid)
-// - Fetch MSBs (32-bit instr and hwb nonvalid)
-// For unaligned jump/flush, CIR MSBs will always be invalid, so
-// we don't care about its value in this case.
-always @ (posedge clk or negedge rst_n) begin
-	if (!rst_n) begin
-		cir <= 32'h0;
-		cir_vld <= 2'h0;
-		hwbuf <= {W_BUNDLE{1'b0}};
-		hwbuf_vld <= 1'b0;
-	end else begin
-		// Update CIR contents
-		if (cir_rdy || jump_target_vld || unaligned_jump_reg) begin
-			case (1'b1)
-				unaligned_jump_dph              : cir[0 +: W_BUNDLE] <= fetch_data[W_BUNDLE +: W_BUNDLE];
-				cir_rdy[1] && !hwbuf_vld : cir[0 +: W_BUNDLE] <= fetch_data[0 +: W_BUNDLE];
-				cir_rdy[1]                      : cir[0 +: W_BUNDLE] <= hwbuf;
-				default                         : cir[0 +: W_BUNDLE] <= cir[W_BUNDLE +: W_BUNDLE];
-			endcase
-			case (1'b1)
-				cir_rdy[1] ~^ hwbuf_vld  : cir[W_BUNDLE +: W_BUNDLE] <= fetch_data[0 +: W_BUNDLE];
-				cir_rdy[1]                      : cir[W_BUNDLE +: W_BUNDLE] <= fetch_data[W_BUNDLE +: W_BUNDLE];
-				default                         : cir[W_BUNDLE +: W_BUNDLE] <= hwbuf;
-			endcase
-		end
-		// Update CIR flags
-		
+		{hwbuf, cir} <= instr_data_plus_fetch;
 	end
 end
 
