4b303e9b9251434a5ddb0b7ce656c5c253bfc74f
Luke Wren <wren6991@gmail.com>
2018-11-11 02:03:04 +0000

    Rewrite sync sram controller. Turns out this was the source of huge cell count for full-system synth. Has revealed faulty assumptions in other hardware wrt how data is arranged on AHB-lite byte lanes (helps to read the spec properly).
    Former-commit-id: 6da6dfbe1acb818c6a4e6f9ceb893791bf4de528
    Former-commit-id: d23b4e29a0444bf058e0dbda30d9c31f227f7e8e

--- hdl/mem/ahb_sync_sram.v
+++ hdl/mem/ahb_sync_sram.v
@@ -15,126 +15,67 @@
  *                                                                    *
  *********************************************************************/
 
+// TODO: nonblocking deferred writes, with write->read bypass on address match
+// (remove wait states)
+
 module ahb_sync_sram #(
 	parameter W_DATA = 32,
 	parameter W_ADDR = 32,
-	parameter DEPTH = 1 << 10
+	parameter DEPTH = 1 << 11
 ) (
 	// Globals
 	input wire clk,
 	input wire rst_n,
 
 	// AHB lite slave interface
-	output wire              ahbls_hready_resp,
-	output wire              ahbls_hresp,
-	input wire [W_ADDR-1:0]  ahbls_haddr,
-	input wire               ahbls_hwrite,
-	input wire [1:0]         ahbls_htrans,
-	input wire [2:0]         ahbls_hsize,
-	input wire [2:0]         ahbls_hburst,
-	input wire [3:0]         ahbls_hprot,
-	input wire               ahbls_hmastlock,
-	input wire [W_DATA-1:0]  ahbls_hwdata,
-	output reg [W_DATA-1:0]  ahbls_hrdata
+	output wire               ahbls_hready_resp,
+	output wire               ahbls_hresp,
+	input  wire [W_ADDR-1:0]  ahbls_haddr,
+	input  wire               ahbls_hwrite,
+	input  wire [1:0]         ahbls_htrans,
+	input  wire [2:0]         ahbls_hsize,
+	input  wire [2:0]         ahbls_hburst,
+	input  wire [3:0]         ahbls_hprot,
+	input  wire               ahbls_hmastlock,
+	input  wire [W_DATA-1:0]  ahbls_hwdata,
+	output wire [W_DATA-1:0]  ahbls_hrdata
 );
-
-integer i;
 
 // This should be localparam but ISIM won't allow the $clog2 call for localparams
 // because of "reasons"
-parameter W_SRAM_ADDR = $clog2(DEPTH);
-parameter MAX_HSIZE = $clog2(W_DATA) - 3;
-localparam W_BYTES = W_DATA / 8;
+parameter  W_SRAM_ADDR = $clog2(DEPTH);
+localparam W_BYTES     = W_DATA / 8;
+parameter  W_BYTEADDR  = $clog2(W_BYTES);
 
-localparam STATE_IDLE = 2'h0;
-localparam STATE_READ = 2'h1;
-localparam STATE_WRITE = 2'h2;
-
-reg  [W_BYTES-1:0] w_mask_noshift;
-wire [W_BYTES-1:0] w_mask;
-
-// NB: we assume little-endian addressing here
-
-reg [2:0]        hsize_d;
-always @ (*) begin
-	for (i = 0; i < W_BYTES; i = i + 1) begin
-		w_mask_noshift[i] = i < (1 << hsize_d);
-	end
-end
-assign w_mask = w_mask_noshift << haddr_d[$clog2(W_BYTES)-1:0];
-
-// Byte swizzling for read/write data
-wire [W_DATA-1:0] sram_rdata;
-reg  [W_DATA-1:0] sram_rdata_rot;
-
-always @ (*) begin
-	for (i = 0; i < W_BYTES; i = i + 1) begin
-		sram_rdata_rot[i * 8 +: 8] = sram_rdata[((i + haddr_d[$clog2(W_BYTES)-1:0]) % W_BYTES) * 8 +: 8];
-	end
-	if (state == STATE_READ) begin
-		for (i = 0; i < W_BYTES; i = i + 1) begin
-			ahbls_hrdata[i * 8 +: 8] = sram_rdata_rot[(i % (1 << hsize_d)) * 8 +: 8];
-		end
-	end else begin
-		ahbls_hrdata = {W_DATA{1'b0}};
-	end
-end
-
-reg [W_DATA-1:0] sram_wdata;
-
-always @ (*) begin
-	for (i = 0; i < W_BYTES; i = i + 1) begin
-		//sram_wdata[i * 8 +: 8] = ahbls_hwdata[(i % (1 << hsize_d)) * 8 +: 8];
-		// Assume that master performs this data replication.
-		// TODO: check this assumption and remove this block
-		sram_wdata[i * 8 +: 8] = ahbls_hwdata[i * 8 +: 8];
-	end
-end
-
-// =================
-// AHB State Machine
-// =================
-
-
-// There is a timing issue with a write followed immediately by a read: for SRAM, write data and
-// address are presented on same clock. For AHB they are different phases. We would therefore
-// need to be able to access two addresses at once to perform read and write at same time!
-// The same timing issue creates an idle cycle when read is followed by write, so one option is to
-// buffer the final write until this idle cycle is reached, and bypass in the buffered write if
-// AHB tries to access the address targeted by this write.
-// For simplicity, we are just inserting an AHB wait state.
-// TODO: do the other thing
-
-reg [1:0] state;
-reg [W_ADDR-1:0] haddr_d;
-
-assign ahbls_hready_resp = state != STATE_WRITE;
 assign ahbls_hresp = 1'b0;
 
-wire [W_ADDR-1:0] sram_addr_full = state == STATE_WRITE ? haddr_d : ahbls_haddr;
-wire [W_SRAM_ADDR-1:0] sram_addr = sram_addr_full[MAX_HSIZE +: W_SRAM_ADDR];
+wire [W_BYTES-1:0] wmask_noshift = ~({W_BYTES{1'b0}} << (1 << ahbls_hsize));
+wire [W_BYTES-1:0] wmask = wmask_noshift << ahbls_haddr[W_BYTEADDR-1:0];
+reg [W_BYTES-1:0] wen;
+
+reg [W_SRAM_ADDR-1:0] addr_saved;
+wire [W_SRAM_ADDR-1:0] sram_addr = hreadyout ?
+	ahbls_haddr[W_BYTEADDR +: W_SRAM_ADDR] : addr_saved;
+
+reg hreadyout;
+assign ahbls_hready_resp = hreadyout;
 
 always @ (posedge clk or negedge rst_n) begin
 	if (!rst_n) begin
-		state <= STATE_IDLE;
-		haddr_d <= {W_ADDR{1'b0}};
-		hsize_d <= 3'h0;
+		hreadyout <= 1'b1;
+		wen <= {W_BYTES{1'b0}};
+		addr_saved <= {W_SRAM_ADDR{1'b0}};
 	end else begin
-		if (state == STATE_WRITE) begin
-			state <= STATE_IDLE;
-		end
-		if (ahbls_hready_resp) begin
-			if (ahbls_htrans[1]) begin
-				state <= ahbls_hwrite ? STATE_WRITE : STATE_READ;
-				hsize_d <= ahbls_hsize;
-				haddr_d <= ahbls_haddr;
-			end else begin
-				state <= STATE_IDLE;
-			end
+		if (ahbls_hwrite && ahbls_htrans[1] && hreadyout) begin
+			hreadyout <= 1'b0;
+			wen <= wmask;
+			addr_saved <= ahbls_haddr[W_BYTEADDR +: W_SRAM_ADDR];
+		end else begin
+			hreadyout <= 1'b1;
+			wen <= {W_BYTES{1'b0}};
 		end
 	end
 end
-
 
 sram_sync #(
 	.WIDTH(W_DATA),
@@ -142,10 +83,10 @@ sram_sync #(
 	.BYTE_ENABLE(1)
 ) sram (
 	.clk   (clk),
-	.wen   (state == STATE_WRITE ? w_mask : {W_BYTES{1'b0}}),
+	.wen   (wen),
 	.addr  (sram_addr),
-	.wdata (sram_wdata),
-	.rdata (sram_rdata)
+	.wdata (ahbls_hwdata),
+	.rdata (ahbls_hrdata)
 );
 
 endmodule
