353861aabbc3c93e81661f99cdfb72427a07816b
Luke Wren <wren6991@gmail.com>
2019-01-20 17:59:29 +0000

    Add simple address translation to vhexwidth. Move software to separate directory.

--- software/include/addressmap.h
+++ software/include/addressmap.h
@@ -1,8 +1,15 @@
 #ifndef _ADDRESSMAP_H_
 #define _ADDRESSMAP_H_
 
-#define SRAM_BASE (0x2 << 28)
-#define PERI_BASE (0x4 << 28)
+// External SRAM/main memory:
+#define SRAM0_BASE (0x2ul << 28)
+#define SRAM0_SIZE (512 * 1024)
+
+// Boot/stack SRAM:
+#define SRAM1_BASE (SRAM0_BASE + SRAM0_SIZE)
+#define SRAM1_SIZE (8 * 1024)
+
+#define PERI_BASE (0x4ul << 28)
 #define GPIO_BASE (PERI_BASE + 0x0000)
 #define UART_BASE (PERI_BASE + 0x1000)
 #define TBMAN_BASE (PERI_BASE + 0xf000)
