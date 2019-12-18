f09ead9276196777e54b569df0c832830f186733
Luke Wren <wren6991@gmail.com>
2019-12-18 13:33:10 +0000

    Fix typo and simulation issue in onehot_priority_dynamic

--- hdl/common/onehot_priority_dynamic.v
+++ hdl/common/onehot_priority_dynamic.v
@@ -1,7 +1,7 @@
 module onehot_priority_dynamic #(
 	parameter N_REQ = 8,
 	parameter N_PRIORITIES = 2,
-	parameter HIGHEST_WINS = 1 // If 1, numerically highest level has greatest priority. Otherwise, level 0 wins.
+	parameter HIGHEST_WINS = 1, // If 1, numerically highest level has greatest priority. Otherwise, level 0 wins.
 	parameter W_PRIORITY = $clog2(N_PRIORITIES) // do not modify
 ) (
 	input wire [N_REQ*W_PRIORITY-1:0] priority,
@@ -42,9 +42,9 @@ reg [N_REQ-1:0] reqs_from_highest_layer;
 
 always @ (*) begin: mux_reqs_by_layer
 	integer i;
-	reqs_from_highest_layer <= {N_REQ{1'b0}};
+	reqs_from_highest_layer = {N_REQ{1'b0}};
 	for (i = 0; i < N_PRIORITIES; i = i + 1)
-		reqs_from_highest_layer <= reqs_from_highest_layer |
+		reqs_from_highest_layer = reqs_from_highest_layer |
 			(req_stratified[i] & {N_REQ{active_layer_sel[i]}});
 end
 
