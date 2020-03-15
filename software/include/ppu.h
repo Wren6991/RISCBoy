8e670a3093c8bbad719f613e10ce012fbbf86062
Luke Wren <wren6991@gmail.com>
2020-03-15 10:48:47 +0000

    Add PPU header changes for spin demo

--- software/include/ppu.h
+++ software/include/ppu.h
@@ -95,6 +95,22 @@ static inline void cproc_put_pc(uint32_t pc)
 
 #define PPU_CPROC_BRANCH_ALWAYS 0x0
 
+#define PPU_FORMAT_ARGB1555 0
+#define PPU_FORMAT_PAL8 1
+#define PPU_FORMAT_PAL4 2
+#define PPU_FORMAT_PAL1
+
+#define PPU_SIZE_8    0
+#define PPU_SIZE_16   1
+#define PPU_SIZE_32   2
+#define PPU_SIZE_64   3
+#define PPU_SIZE_128  4
+#define PPU_SIZE_256  5
+#define PPU_SIZE_512  6
+#define PPU_SIZE_1024 7
+
+#define PPU_ABLIT_FULLSIZE 0
+#define PPU_ABLIT_HALFSIZE 1
 static inline size_t cproc_sync(uint32_t *prog)
 {
 	*prog++ = PPU_CPROC_SYNC;
@@ -116,7 +132,7 @@ static inline size_t cproc_fill(uint32_t *prog, uint8_t r, uint8_t g, uint8_t b)
 static inline size_t cproc_branch(uint32_t *prog, uint32_t target, uint32_t condition, uint16_t compval)
 {
 	*prog++ = PPU_CPROC_JUMP | ((condition & 0xfu) << 24) | (compval & 0x3ffu);
-	*prog++ = target & 0xfffffffeu;
+	*prog++ = target & 0xfffffffcu;
 	return 2;
 }
 
@@ -124,5 +140,23 @@ static inline size_t cproc_jump(uint32_t *prog, uintptr_t target)
 {
 	return cproc_branch(prog, target, PPU_CPROC_BRANCH_ALWAYS, 0);
 }
+
+static inline size_t cproc_blit(uint32_t *prog, uint16_t x, uint16_t y, uint8_t size, uint8_t poff, uint8_t fmt, void *img)
+{
+	*prog++ = PPU_CPROC_BLIT | (x & 0x3ffu) | ((y & 0x3ffu) << 10) | ((size & 0x7u) << 25) | ((poff & 0x7u) << 22);
+	*prog++ = ((uint32_t)img & 0xfffffffcu) | (fmt & 0x3u);
+	return 2;
+}
+
+static inline size_t cproc_ablit(uint32_t *prog, uint16_t x, uint16_t y, uint8_t size, uint8_t poff, uint8_t fmt, uint8_t halfsize, int32_t *aparam, void *img)
+{
+	*prog++ = PPU_CPROC_ABLIT | (x & 0x3ffu) | ((y & 0x3ffu) << 10) | ((size & 0x7u) << 25) | ((poff & 0x7u) << 22) | (!!halfsize << 21);
+	*prog++ = (((uint32_t)aparam[2] >> 10) & 0x0000ffffu) | (((uint32_t)aparam[5] << 6) & 0xffff0000u);
+	*prog++ = (((uint32_t)aparam[0] >> 8 ) & 0x0000ffffu) | (((uint32_t)aparam[1] << 8) & 0xffff0000u);
+	*prog++ = (((uint32_t)aparam[3] >> 8 ) & 0x0000ffffu) | (((uint32_t)aparam[4] << 8) & 0xffff0000u);
+	*prog++ = ((uint32_t)img & 0xfffffffcu) | (fmt & 0x3u);
+	return 5;
+}
+
 
 #endif // _PPU_H_
