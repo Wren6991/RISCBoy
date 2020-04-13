eb8991fd483cc955a7637a96b12625fddaeb62ad
Luke Wren <wren6991@gmail.com>
2020-04-13 12:20:10 +0100

    Port Zelda tile demo, with affine camera

--- software/src/tile_test.c
+++ software/src/tile_test.c
@@ -3,10 +3,15 @@
 #include "ppu.h"
 #include "lcd.h"
 #include "gpio.h"
+#include "affine_transform.h"
+#include "tbman.h"
 
 #include "zelda_tileset_mini_pal8.h"
 #include "zelda_tileset_mini_pal8_palette.h"
 #include "map_test_zelda_mini.h"
+
+#define SCREEN_WIDTH 320u
+#define SCREEN_HEIGHT 240u
 
 uint32_t __attribute__ ((section (".noload"))) cproc_prog[256];
 
@@ -23,21 +28,25 @@ void render_frame()
 
 int main()
 {
-	lcd_init(ili9341_init_seq);
+	if (!tbman_running_in_sim())
+		lcd_init(ili9341_init_seq);
 
-	mm_ppu->dispsize = (319 >> PPU_DISPSIZE_W_LSB) | (239 << PPU_DISPSIZE_H_LSB);
+	mm_ppu->dispsize = (SCREEN_WIDTH - 1 >> PPU_DISPSIZE_W_LSB) | (SCREEN_HEIGHT - 1 << PPU_DISPSIZE_H_LSB);
 
 	for (int i = 0; i < ZELDA_MINI_PALETTE_SIZE; ++i)
 		PPU_PALETTE_RAM[i] = ((const uint16_t *)zelda_tileset_mini_pal8_palette)[i] | 0x8000u;
 
-	unsigned int scroll_x = 0, scroll_y = 0, idle_count = 0;
+	affine_transform_t cam_trans;
+	affine_identity(cam_trans);
+
+	unsigned int idle_count = 0;
 	while (true)
 	{
 		uint32_t *p = cproc_prog;
-		p += cproc_clip(p, 0, 319);
+		p += cproc_clip(p, 0, SCREEN_WIDTH - 1);
 		p += cproc_fill(p, 16, 0, 16);
-		p += cproc_tile(p, -scroll_x, -scroll_y, PPU_SIZE_1024, 0,
-			PPU_FORMAT_PAL8, PPU_SIZE_16, zelda_tileset_mini_pal8, map_test_zelda_mini);
+		p += cproc_atile(p, 0, 0, PPU_SIZE_1024, 0, PPU_FORMAT_PAL8, PPU_SIZE_16,
+			cam_trans, zelda_tileset_mini_pal8, map_test_zelda_mini);
 		p += cproc_sync(p);
 		p += cproc_jump(p, (uintptr_t)cproc_prog);
 		cproc_put_pc((uint32_t)cproc_prog);
@@ -47,13 +56,13 @@ int main()
 		const unsigned SPEED = 3;
 		const unsigned IDLE_PERIOD = 300;
 		if (gpio_in_pin(PIN_DPAD_U))
-			scroll_y -= SPEED;
+			affine_translate(cam_trans, 0, SPEED);
 		if (gpio_in_pin(PIN_DPAD_D))
-			scroll_y += SPEED;
+			affine_translate(cam_trans, 0, -SPEED);
 		if (gpio_in_pin(PIN_DPAD_L))
-			scroll_x -= SPEED;
+			affine_translate(cam_trans, SPEED, 0);
 		if (gpio_in_pin(PIN_DPAD_R))
-			scroll_x += SPEED;
+			affine_translate(cam_trans, -SPEED, 0);
 
 		if (!(gpio_in() & ((1u << PIN_DPAD_U) | (1u << PIN_DPAD_D) | (1u << PIN_DPAD_L) | (1u << PIN_DPAD_R))))
 		{
@@ -65,6 +74,13 @@ int main()
 			idle_count = 0;
 		}
 		if (idle_count >= IDLE_PERIOD)
-			scroll_x += SPEED;
+			affine_translate(cam_trans, -SPEED, 0);
+
+		if (gpio_in() & (1u << PIN_BTN_A))
+		{
+			affine_translate(cam_trans, -(SCREEN_WIDTH / 2), -(SCREEN_HEIGHT / 2));
+			affine_rotate(cam_trans, SPEED);
+			affine_translate(cam_trans, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+		}
 	}
 }
