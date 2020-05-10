e4f68590d543f3df733a5469ef663867e5d837ed
Luke Wren <wren6991@gmail.com>
2020-05-10 20:13:06 +0100

    Get to blinky on ULX3S

---
+++ hdl/riscboy_fpga/riscboy_fpga_ulx3s.f
@@ -0,0 +1,4 @@
+file riscboy_fpga_ulx3s.v
+file $LIBFPGA/common/blinky.v
+file $LIBFPGA/common/fpga_reset.v
+list $HDL/riscboy_core/riscboy_core.f
---
+++ synth/ULX3S.mk
@@ -0,0 +1,29 @@
+CHIPNAME=riscboy_ulx3s
+TOP=riscboy_fpga
+DOTF=$(HDL)/riscboy_fpga/riscboy_fpga_ulx3s.f
+BOOTAPP=blinky
+
+SYNTH_OPT=-abc9
+
+DEVICE=um5g-85k
+PACKAGE=CABGA381
+
+include $(SCRIPTS)/synth_ecp5.mk
+
+# romfiles is a prerequisite for synth
+romfiles::
+	@echo ">>> Bootcode"
+	@echo
+	make -C $(SOFTWARE)/build APPNAME=$(BOOTAPP) CCFLAGS="-Os -DFORCE_SRAM0_SIZE=131072"
+	cp $(SOFTWARE)/build/$(BOOTAPP)8.hex bootram_init8.hex
+	$(SCRIPTS)/vhexwidth bootram_init8.hex -w 32 -b 0x20080000 -o bootram_init32.hex
+
+clean::
+	make -C $(SOFTWARE)/build APPNAME=$(BOOTAPP) clean
+	rm -f bootram_init*.hex
+
+prog: bit
+	ujprog $(CHIPNAME).bit
+
+flash: bit
+	ujprog -j flash $(CHIPNAME).bit
