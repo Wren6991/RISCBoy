7c67af236860997d3b5676c1b3ebfb66b1a4467a
Luke Wren <wren6991@gmail.com>
2020-12-03 04:12:02 +0000

    Port parallax demo to PPU2

--- software/src/parallax_demo.c
+++ software/src/parallax_demo.c
@@ -1,7 +1,7 @@
 #define CLK_SYS_MHZ 36
 
 #include "ppu.h"
-#include "lcd.h"
+#include "display.h"
 #include "gpio.h"
 
 #include "tileset.h"
@@ -9,55 +9,41 @@
 #include "tilemap_background.h"
 #include "tilemap_foreground.h"
 
-void render_frame()
-{
-	lcd_wait_idle();
-	lcd_force_dc_cs(1, 1);
-	st7789_start_pixels();
-
-	mm_ppu->csr = PPU_CSR_HALT_VSYNC_MASK | PPU_CSR_RUN_MASK;
-	while (mm_ppu->csr & PPU_CSR_RUNNING_MASK)
-		;
-}
+uint32_t ppu_prog[64];
 
 int main()
 {
-	lcd_init(ili9341_init_seq);
-
+	display_init();
 	mm_ppu->dispsize = (319 >> PPU_DISPSIZE_W_LSB) | (239 << PPU_DISPSIZE_H_LSB);
-	mm_ppu->default_bg_colour = 0x2e71; // #5e978c
-
-	mm_ppu->bg[0].tsbase = (uint32_t)tileset;
-	mm_ppu->bg[0].tmbase = (uint32_t)tilemap_background;
-	mm_ppu->bg[0].csr =
-		(1u << PPU_BG0_CSR_EN_LSB) |
-		(5u << PPU_BG0_CSR_PFWIDTH_LSB) | // 512 px wide
-		(4u << PPU_BG0_CSR_PFHEIGHT_LSB) | // 256 px high
-		(1u << PPU_BG0_CSR_TILESIZE_LSB) | // 16x16 pixel tiles
-		(1u << PPU_BG0_CSR_TRANSPARENCY_LSB) |
-		(PPU_PIXMODE_ARGB1555 << PPU_BG0_CSR_PIXMODE_LSB);
-
-	mm_ppu->bg[1].tsbase = (uint32_t)tileset;
-	mm_ppu->bg[1].tmbase = (uint32_t)tilemap_foreground;
-	mm_ppu->bg[1].csr =
-		(1u << PPU_BG0_CSR_EN_LSB) |
-		(6u << PPU_BG0_CSR_PFWIDTH_LSB) | // 1024 px wide
-		(4u << PPU_BG0_CSR_PFHEIGHT_LSB) | // 256 px high
-		(1u << PPU_BG0_CSR_TILESIZE_LSB) | // 16x16 pixel tiles
-		(1u << PPU_BG0_CSR_TRANSPARENCY_LSB) |
-		(PPU_PIXMODE_ARGB1555 << PPU_BG0_CSR_PIXMODE_LSB);
-
-	for (int i = 0; i < PALETTE_N_COLOURS; ++i)
-		PPU_PALETTE_RAM[i] = ((const uint16_t *)tileset_palette)[i];
 
 	unsigned scroll_fg = 0;
 	unsigned scroll_bg = 0;
 	while (1)
 	{
-		render_frame();
+		uint32_t *p = ppu_prog;
+		p += cproc_clip(p, 0, 319);
+		p += cproc_fill(p, 13, 18, 17); // #5e978c
+		p += cproc_tile(p, -scroll_bg, 0,
+			PPU_SIZE_512,
+			0, PPU_FORMAT_ARGB1555,
+			PPU_SIZE_16,
+			tileset,
+			tilemap_background
+		);
+		p += cproc_tile(p, -scroll_fg, 0,
+			PPU_SIZE_1024,
+			0, PPU_FORMAT_ARGB1555,
+			PPU_SIZE_16,
+			tileset,
+			tilemap_foreground
+		);
+		p += cproc_sync(p);
+		p += cproc_jump(p, ppu_prog);
+		cproc_put_pc(ppu_prog);
+
+		display_start_frame();
+		display_wait_frame_end();
 		scroll_fg += 2;
 		scroll_bg += 1;
-		mm_ppu->bg[0].scroll = scroll_bg & PPU_BG0_SCROLL_X_MASK;
-		mm_ppu->bg[1].scroll = scroll_fg & PPU_BG0_SCROLL_X_MASK;
 	}
 }
