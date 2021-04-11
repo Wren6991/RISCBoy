6640a94193079397f6ae2ee970665a394910c98e
Luke Wren <wren6991@gmail.com>
2021-04-12 00:11:50 +0100

    Update execution tb for dual-port processor

--- test/execution/tb.v
+++ test/execution/tb.v
@@ -11,142 +11,108 @@ reg                       clk;
 reg                       rst_n;
 reg                       rst_n_proc;
 
-wire                      cpu_hready;
-wire                      cpu_hresp;
-wire [W_ADDR-1:0]         cpu_haddr;
-wire                      cpu_hwrite;
-wire [1:0]                cpu_htrans;
-wire [2:0]                cpu_hsize;
-wire [2:0]                cpu_hburst;
-wire [3:0]                cpu_hprot;
-wire                      cpu_hmastlock;
-wire [W_DATA-1:0]         cpu_hwdata;
-wire [W_DATA-1:0]         cpu_hrdata;
+wire                      i_hready = 1'b1;
+wire                      i_hresp = 1'b0;
+wire [W_ADDR-1:0]         i_haddr;
+wire                      i_hwrite;
+wire [1:0]                i_htrans;
+wire [2:0]                i_hsize;
+wire [2:0]                i_hburst;
+wire [3:0]                i_hprot;
+wire                      i_hmastlock;
+wire [W_DATA-1:0]         i_hwdata;
+wire [W_DATA-1:0]         i_hrdata;
 
-wire                      trafficgen_hready;
-wire                      trafficgen_hresp;
-wire [W_ADDR-1:0]         trafficgen_haddr;
-wire                      trafficgen_hwrite;
-wire [1:0]                trafficgen_htrans;
-wire [2:0]                trafficgen_hsize;
-wire [2:0]                trafficgen_hburst;
-wire [3:0]                trafficgen_hprot;
-wire                      trafficgen_hmastlock;
-wire [W_DATA-1:0]         trafficgen_hwdata;
-wire [W_DATA-1:0]         trafficgen_hrdata;
+wire                      d_hready = 1'b1;
+wire                      d_hresp = 1'b0;
+wire [W_ADDR-1:0]         d_haddr;
+wire                      d_hwrite;
+wire [1:0]                d_htrans;
+wire [2:0]                d_hsize;
+wire [2:0]                d_hburst;
+wire [3:0]                d_hprot;
+wire                      d_hmastlock;
+wire [W_DATA-1:0]         d_hwdata;
+wire [W_DATA-1:0]         d_hrdata;
 
-wire                      sram_hready_resp;
-wire                      sram_hready;
-wire                      sram_hresp;
-wire [W_ADDR-1:0]         sram_haddr;
-wire                      sram_hwrite;
-wire [1:0]                sram_htrans;
-wire [2:0]                sram_hsize;
-wire [2:0]                sram_hburst;
-wire [3:0]                sram_hprot;
-wire                      sram_hmastlock;
-wire [W_DATA-1:0]         sram_hwdata;
-wire [W_DATA-1:0]         sram_hrdata;
 
-hazard5_cpu #(
+hazard5_cpu_2port #(
 	.RESET_VECTOR(32'h0000_0000)
 ) cpu0 (
 	.clk             (clk),
 	.rst_n           (rst_n_proc),
 
-	.ahblm_hready    (cpu_hready),
-	.ahblm_hresp     (cpu_hresp),
-	.ahblm_haddr     (cpu_haddr),
-	.ahblm_hwrite    (cpu_hwrite),
-	.ahblm_htrans    (cpu_htrans),
-	.ahblm_hsize     (cpu_hsize),
-	.ahblm_hburst    (cpu_hburst),
-	.ahblm_hprot     (cpu_hprot),
-	.ahblm_hmastlock (cpu_hmastlock),
-	.ahblm_hwdata    (cpu_hwdata),
-	.ahblm_hrdata    (cpu_hrdata),
+	.i_hready        (i_hready),
+	.i_hresp         (i_hresp),
+	.i_haddr         (i_haddr),
+	.i_hwrite        (i_hwrite),
+	.i_htrans        (i_htrans),
+	.i_hsize         (i_hsize),
+	.i_hburst        (i_hburst),
+	.i_hprot         (i_hprot),
+	.i_hmastlock     (i_hmastlock),
+	.i_hwdata        (i_hwdata),
+	.i_hrdata        (i_hrdata),
+
+	.d_hready        (d_hready),
+	.d_hresp         (d_hresp),
+	.d_haddr         (d_haddr),
+	.d_hwrite        (d_hwrite),
+	.d_htrans        (d_htrans),
+	.d_hsize         (d_hsize),
+	.d_hburst        (d_hburst),
+	.d_hprot         (d_hprot),
+	.d_hmastlock     (d_hmastlock),
+	.d_hwdata        (d_hwdata),
+	.d_hrdata        (d_hrdata),
 
 	.irq             (16'h0)
 );
 
-trafficgen #(
-	.W_ADDR (W_ADDR),
-	.W_DATA (W_DATA),
-	.TARGET_ADDR (0),
-	.IDLENESS (2)
-) trafficgen0 (
-	.clk             (clk),
-	.rst_n           (rst_n_proc),
-
-	.ahblm_hready    (trafficgen_hready),
-	.ahblm_hresp     (trafficgen_hresp),
-	.ahblm_haddr     (trafficgen_haddr),
-	.ahblm_hwrite    (trafficgen_hwrite),
-	.ahblm_htrans    (trafficgen_htrans),
-	.ahblm_hsize     (trafficgen_hsize),
-	.ahblm_hburst    (trafficgen_hburst),
-	.ahblm_hprot     (trafficgen_hprot),
-	.ahblm_hmastlock (trafficgen_hmastlock),
-	.ahblm_hwdata    (trafficgen_hwdata),
-	.ahblm_hrdata    (trafficgen_hrdata)
-);
-
-ahbl_arbiter #(
-		.N_PORTS(2),
-		.W_ADDR(W_ADDR),
-		.W_DATA(W_DATA)
-	) arbiter0 (
-	.clk             (clk),
-	.rst_n           (rst_n),
-	.src_hready      ({cpu_hready    , trafficgen_hready   }), // trafficgen has higher priority (since its purpose is to generate stall signals!
-	.src_hready_resp ({cpu_hready    , trafficgen_hready   }),
-	.src_hresp       ({cpu_hresp     , trafficgen_hresp    }),
-	.src_haddr       ({cpu_haddr     , trafficgen_haddr    }),
-	.src_hwrite      ({cpu_hwrite    , trafficgen_hwrite   }),
-	.src_htrans      ({cpu_htrans    , trafficgen_htrans   }),
-	.src_hsize       ({cpu_hsize     , trafficgen_hsize    }),
-	.src_hburst      ({cpu_hburst    , trafficgen_hburst   }),
-	.src_hprot       ({cpu_hprot     , trafficgen_hprot    }),
-	.src_hmastlock   ({cpu_hmastlock , trafficgen_hmastlock}),
-	.src_hwdata      ({cpu_hwdata    , trafficgen_hwdata   }),
-	.src_hrdata      ({cpu_hrdata    , trafficgen_hrdata   }),
-	.dst_hready      (sram_hready),
-	.dst_hready_resp (sram_hready_resp),
-	.dst_hresp       (sram_hresp),
-	.dst_haddr       (sram_haddr),
-	.dst_hwrite      (sram_hwrite),
-	.dst_htrans      (sram_htrans),
-	.dst_hsize       (sram_hsize),
-	.dst_hburst      (sram_hburst),
-	.dst_hprot       (sram_hprot),
-	.dst_hmastlock   (sram_hmastlock),
-	.dst_hwdata      (sram_hwdata),
-	.dst_hrdata      (sram_hrdata)
-);
+reg [31:0] sram[0:SRAM_SIZE_BYTES/4-1];
+initial $readmemh("../ram_init32.hex", sram);
 
 
-ahb_sync_sram #(
-	.W_DATA(32),
-	.W_ADDR(32),
-	.DEPTH(SRAM_SIZE_BYTES / 4),
-	.PRELOAD_FILE("../ram_init32.hex")
-) sram0 (
-	.clk(clk),
-	.rst_n(rst_n),
+// Dual-ported memory model:
 
-	.ahbls_hready      (sram_hready),
-	.ahbls_hready_resp (sram_hready_resp),
-	.ahbls_hresp       (sram_hresp),
-	.ahbls_haddr       (sram_haddr),
-	.ahbls_hwrite      (sram_hwrite),
-	.ahbls_htrans      (sram_htrans),
-	.ahbls_hsize       (sram_hsize),
-	.ahbls_hburst      (sram_hburst),
-	.ahbls_hprot       (sram_hprot),
-	.ahbls_hmastlock   (sram_hmastlock),
-	.ahbls_hwdata      (sram_hwdata),
-	.ahbls_hrdata      (sram_hrdata)
-);
+reg              i_active_dphase;
+reg [W_ADDR-1:0] i_haddr_dphase;
+
+reg              d_active_dphase;
+reg              d_hwrite_dphase;
+reg [2:0]        d_hsize_dphase;
+reg [W_ADDR-1:0] d_haddr_dphase;
+
+assign i_hrdata = i_active_dphase &&                     i_hready ? sram[i_haddr_dphase / 4] : {W_DATA{1'bx}};
+assign d_hrdata = d_active_dphase && !d_hwrite_dphase && d_hready ? sram[d_haddr_dphase / 4] : {W_DATA{1'bx}};
+
+always @ (posedge clk or negedge rst_n) begin: ram_model
+	integer i;
+	if (!rst_n) begin
+		i_active_dphase <= 1'b0;
+		i_haddr_dphase <= {W_ADDR{1'b0}};
+		d_active_dphase <= 1'b0;
+		d_hwrite_dphase <= 1'b0;
+		d_hsize_dphase <= 3'h0;
+		d_haddr_dphase <= {W_ADDR{1'b0}};
+	end else begin
+		if (i_hready) begin
+			i_active_dphase <= i_htrans[1];
+			i_haddr_dphase <= i_haddr;
+		end
+		if (d_hready) begin
+			d_active_dphase <= d_htrans[1];
+			d_hwrite_dphase <= d_hwrite;
+			d_hsize_dphase <= d_hsize;
+			d_haddr_dphase <= d_haddr;
+		end
+		if (d_active_dphase && d_hwrite_dphase) begin
+			for (i = d_haddr_dphase; i < d_haddr_dphase + (1 << d_hsize_dphase); i = i + 1) begin
+				sram[i / 4][i % 4 * 8 +: 8] <= d_hwdata[i % 4 * 8 +: 8];
+			end
+		end
+	end
+end
 
 always #(CLK_PERIOD * 0.5) clk = !clk;
 
@@ -169,7 +135,7 @@ initial begin
 
 	$display(".testdata preload:");
 	for (i = result_base_ptr; i < result_end_ptr; i = i + 4)
-		$display("%h", sram0.sram.behav_mem.mem [i / 4]);
+		$display("%h", sram[i / 4]);
 
 
 	#(10 * CLK_PERIOD);
@@ -180,7 +146,7 @@ initial begin
 	
 	$display("Register contents:");
 	for (i = 0; i < 32; i = i + 1) begin
-		$display("%h", cpu0.inst_regfile_1w2r.\real_dualport_reset.mem [i]);
+		$display("%h", cpu0.core.inst_regfile_1w2r.\real_dualport_reset.mem [i]);
 	end
 
 	// Reset the processor (+ trafficgen) and wait a couple cycles.
@@ -192,7 +158,7 @@ initial begin
 
 	$display("Test results:");
 	for (i = result_base_ptr; i < result_end_ptr; i = i + 4)
-		$display("%h", sram0.sram.behav_mem.mem [i / 4]);
+		$display("%h", sram[i / 4]);
 
 	$finish(2);
 end
