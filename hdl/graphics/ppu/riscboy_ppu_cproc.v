bad433282c24cc5073cdfee3260d56edf657c1f7
Luke Wren <wren6991@gmail.com>
2025-12-01 22:54:28 +0000

    Fix size-1 spans not being rendered (same fix as 25f8bb6eae on main branch)

--- hdl/graphics/ppu/riscboy_ppu_cproc.v
+++ hdl/graphics/ppu/riscboy_ppu_cproc.v
@@ -285,8 +285,8 @@ wire [INSTR_X_BITS:0] xr_clip_primary =   min(test_xr, clip_x1);
 wire [INSTR_X_BITS:0] xl_clip_secondary = max(test_xl, {{INSTR_X_BITS+1-W_COORD_SX{1'b0}}, clip_x0} | {1'b1, {INSTR_X_BITS{1'b0}}});
 wire [INSTR_X_BITS:0] xr_clip_secondary = min(test_xr, {{INSTR_X_BITS+1-W_COORD_SX{1'b0}}, clip_x1} | {1'b1, {INSTR_X_BITS{1'b0}}});
 
-wire clip_pass_primary = xl_clip_primary < xr_clip_primary;
-wire clip_pass_secondary = xl_clip_secondary < xr_clip_secondary;
+wire clip_pass_primary = xl_clip_primary <= xr_clip_primary;
+wire clip_pass_secondary = xl_clip_secondary <= xr_clip_secondary;
 wire [INSTR_Y_BITS:0] blit_y_offs = {1'b0, {{INSTR_Y_BITS-W_COORD_SY{1'b0}}, beam_y} - instr[INSTR_Y_LSB +: INSTR_Y_BITS]};
 // Note <= not <, because blit_size is end - start, not the pixel count (off by one)
 wire blit_intersects_y = !(blit_y_offs > blit_size);
